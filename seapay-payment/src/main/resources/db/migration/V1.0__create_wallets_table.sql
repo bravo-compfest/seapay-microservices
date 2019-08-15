f0ef1d5e49d6b55a1cf04c97a490a67ad4ab49d1
Erma Safira Nurmasyita <erma.nurmasyita@aux.go-jek.com>
2019-08-15 23:35:09 +0700

    Add migrations

---
+++ seapay-payment/src/main/resources/db/migration/V1.0__create_wallets_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE wallets
+(
+  id int NOT NULL,
+  user_id int NOT NULL,
+  balance DECIMAL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE transactions
+(
+    id serial PRIMARY KEY,
+    sender_id int NOT NULL REFERENCES wallets(id),
+    receiver_id int NOT NULL REFERENCES wallets(id),
+    balance DECIMAL,
+    created_at TIMESTAMP
+);
---
+++ seapay-user-management/src/main/resources/db/migration/V1.0__init_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE customers
+(
+    id serial PRIMARY KEY,
+    name int NOT NULL,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    email VARCHAR(50),
+    phone_number VARCHAR(20),
+    loyalty_level int
+);
+
+CREATE TABLE merchants
+(
+    id serial PRIMARY KEY,
+    name int NOT NULL,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    store_name VARCHAR(50),
+    description VARCHAR(100)
+);
