afad27835393d63ff7816c9b08477616f6e205b5
seagalputra <seagalputra@gmail.com>
2019-08-18 00:40:24 +0700

    [dio] add cors in controller

--- seapay-payment/src/main/java/com/bravo/seapaypayment/controller/WalletController.java
+++ seapay-payment/src/main/java/com/bravo/seapaypayment/controller/WalletController.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin
 @RestController
 public class WalletController {
     @Autowired
